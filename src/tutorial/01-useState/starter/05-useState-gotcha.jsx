f784357cb9287d56d8583a7552129d98edff2668
LevisOgembo <ogembolevis97@gmail.com>
2026-02-11 19:35:36 +0300

    implemented useeffect

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value,setValue] = useState(0)
   const handleClick = () => {
-    setValue(value+1)
+    setTimeout(()=>{
+      setValue((currentState)=>{
+        return currentState + 1
+      })
+    },3000)
   }
   return <div>
     <h1>useState "gotcha"</h1>
