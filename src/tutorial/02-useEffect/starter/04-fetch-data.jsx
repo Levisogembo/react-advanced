62507ef1217cc5ce3763ecf77b3f884f12cb1b9b
LevisOgembo <ogembolevis97@gmail.com>
2026-02-12 19:44:54 +0300

    added useeffects with fetching data from apis

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetch(url)
+        const jsonData = await data.json()
+        setUsers(jsonData)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData()
+  }, [])
+
+  const logId = (node_id) => {
+    console.log(node_id);
+  }
+  return (
+    <section>
+      <h3>Github users</h3>
+      <ul className='users'>{users.map((user) => {
+        //console.log(user);
+        const { id, login, avatar_url, node_id, html_url } = user
+        return <li key={id}>
+          <img src={avatar_url} ></img>
+          <p>{login}</p>
+          <div><a href={html_url}>Profile</a></div>
+          <div><button type="button" className="btn" onClick={() => logId(node_id)}>Click Me</button></div>
+        </li>
+      })}</ul>
+    </section>
+  );
 };
 export default FetchData;
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,30 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState(null)
+  const [isLoading,setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch(url)
+      const data = await res.json()
+      console.log(data);
+      setUser(data)
+      setTimeout(() => {
+        setLoading(!isLoading)
+      },1000);
+    }
+    fetchUser()
+  }, [])
+  if(isLoading) return <h2>Loading...</h2>
+  const {id, avatar_url,login} = user
+  return (
+    <section>
+      <div key={id}>
+        <img src={avatar_url}></img>
+        <p>{login}</p>
+      </div>
+    </section>
+  )
 };
 export default MultipleReturnsFetchData;
