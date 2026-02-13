2ef73d9db6a3178e0dc524cdf85ea31de579f854
LevisOgembo <ogembolevis97@gmail.com>
2026-02-13 17:54:49 +0300

    implemented conditional rendering with short circuits

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -3,27 +3,39 @@ const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState(null)
-  const [isLoading,setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true)
+  const [isError, setError] = useState(false)
 
   useEffect(() => {
     const fetchUser = async () => {
-      const res = await fetch(url)
-      const data = await res.json()
-      console.log(data);
-      setUser(data)
-      setTimeout(() => {
-        setLoading(!isLoading)
-      },1000);
+      try {
+        const res = await fetch(url)
+        if(!res.ok){
+          setError(true)
+          setLoading(false)
+          return
+        }
+        const data = await res.json()
+        //console.log(res);
+        setUser(data)
+        setTimeout(() => {
+          setLoading(!isLoading)
+        }, 1000);
+      } catch (error) {
+        setError(true)
+      }
     }
     fetchUser()
   }, [])
-  if(isLoading) return <h2>Loading...</h2>
-  const {id, avatar_url,login} = user
+  if (isLoading) return <h2>Loading...</h2>
+  if (isError) return <h2>There was an error</h2>
+  const { id, avatar_url, login, company } = user
   return (
     <section>
       <div key={id}>
-        <img src={avatar_url}></img>
+        <img style={{width:'150px', borderRadius: '25px' }} alt={login} src={avatar_url}></img>
         <p>{login}</p>
+        <h2>works at: {company}</h2>
       </div>
     </section>
   )
--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -8,7 +8,7 @@ const ShortCircuitExamples = () => {
   const [user, setUser] = useState({ name: 'john' });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return <p>{text || 'default'}</p>;
 };
 
 export default ShortCircuitExamples;
